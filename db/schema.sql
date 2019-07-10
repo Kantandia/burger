9bb1f0fdba3bbdb2a4946bbea2ceb37ef3bafcba
Kantandia <taylorsdsmith@gmail.com>
2019-07-10 09:59:03 -0500

    Began structure

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burger_DB;
+
+CREATE DATABASE burger_DB;
+
+USE burger_DB;
+
+CREATE TABLE burgers(
+id NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR (50) NOT NULL,
+
+
+
+
+)
