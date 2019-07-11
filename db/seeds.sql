fb690a5c05cc7864e2ec49882e6222fbe8819e1d
Kantandia <taylorsdsmith@gmail.com>
2019-07-11 16:18:14 -0500

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO Burgers (id, name, devoured, date)
+VALUES
+
+(1, 'Hamburger', 1, '2019-7-11 16:10:11'),
+	(2, 'Cheese Burger', 1, '2019-7-11 16:11:15'),
+	(3, 'Double Bacon Burger', 0, '2019-7-11 16:12:12'),
+	(4, 'Veggie Burger', 0, '2019-7-11 16:13:22'),
+	(5, 'Impossible Burger', 0, '2019-7-11 16:14:24');
