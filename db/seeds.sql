eced880a710fab97bf2972ca6a590e305902f270
Kantandia <50807492+Kantandia@users.noreply.github.com>
2019-07-11 19:35:39 -0500

    Time stamp wasn't working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,6 @@
-INSERT INTO Burgers (id, name, devoured, date)
-VALUES
+INSERT INTO burgers (burger_name,devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double-Double', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double Bacon Cheeseburger', FALSE);
 
-(1, 'Hamburger', 1, '2019-7-11 16:10:11'),
-	(2, 'Cheese Burger', 1, '2019-7-11 16:11:15'),
-	(3, 'Double Bacon Burger', 0, '2019-7-11 16:12:12'),
-	(4, 'Veggie Burger', 0, '2019-7-11 16:13:22'),
-	(5, 'Impossible Burger', 0, '2019-7-11 16:14:24');
+TRUNCATE TABLE burgers;
